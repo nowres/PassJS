e695c448e7bdb2149a3536075385fbfb26eff51f
Nowres RAFID <nowres.rafed@gmail.com>
2012-04-22 17:49:28 +0000

    adding tests to build system

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ MINIFIER=yui-compressor
 echo "/* PassJS 0.1.0 pre-alpha */" > bin/pass.min.js
 $MINIFIER src/Array.js >> bin/pass.min.js
 $MINIFIER src/String.js >> bin/pass.min.js
+
+echo "/* PassJS 0.1.0 pre-alpha */" > bin/pass.test.min.js
+$MINIFIER tests/Array.js >> bin/pass.test.min.js
+$MINIFIER tests/String.js >> bin/pass.test.min.js
+$MINIFIER tests/pass.test.js >> bin/pass.test.min.js
