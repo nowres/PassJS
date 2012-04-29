5a15aa3aefa3cbe6a2c4ab55967a8b7cfd9c65fd
Nowres RAFID <nowres.rafed@gmail.com>
2012-04-29 20:22:29 +0000

    Adding String.trimLeft and String.trimRight

--- tests/String.js
+++ tests/String.js
@@ -19,9 +19,33 @@ function test_string_trim() {
     ok(s.trim(), "test", "String.trim 4");
 }
 
+function test_string_trimRight() {
+    var s = "  test    ";
+    ok(s.trimRight(), "  test", "String.trimRight 1");
+    s = "";
+    ok(s.trimRight(), "", "String.trimRight 2");
+    s = "   test";
+    ok(s.trimRight(), "   test", "String.trimRight 3");
+    s = "test       ";
+    ok(s.trimRight(), "test", "String.trimRight 4");
+}
+
+function test_string_trimLeft() {
+    var s = "  test    ";
+    ok(s.trimLeft(), "test    ", "String.trimLeft 1");
+    s = "";
+    ok(s.trimLeft(), "", "String.trimLeft 2");
+    s = "   test";
+    ok(s.trimLeft(), "test", "String.trimLeft 3");
+    s = "test       ";
+    ok(s.trimLeft(), "test       ", "String.trimLeft 4");
+}
+
 
 function test_string() {
     info('Testing String');
-    test_array_map();
+    test_string_trim();
+    test_string_trimRight();
+    test_string_trimLeft();
     info('Testing String done');
 }
