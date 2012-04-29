43633232ba44f95b09ef86f1387fe0616ccc7532
Nowres RAFID <nowres.rafed@gmail.com>
2012-04-29 20:24:00 +0000

    Revert "Adding String.trimLeft and String.trimRight"
    This reverts commit 5a15aa3aefa3cbe6a2c4ab55967a8b7cfd9c65fd.

--- src/String.js
+++ src/String.js
@@ -10,11 +10,3 @@
 String.prototype.trim = function () {
     return this.replace(/^\s+|\s+$/g,'');
 };
-
-String.prototype.trimRight = function () {
-    return this.replace(/\s+$/g,'');
-};
-
-String.prototype.trimLeft = function () {
-    return this.replace(/^\s+/g,'');
-};
