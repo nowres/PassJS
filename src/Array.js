a8c39b4e9d8095a919d364cdb1857f4c0660bb68
Nowres RAFID <nowres.rafed@gmail.com>
2012-05-16 14:19:11 +0000

    Adding conditional definition

--- src/Array.js
+++ src/Array.js
@@ -7,86 +7,103 @@
  */
 
 /* TODO: sobj */
-Array.prototype.forEach = function(func, sobj) {
-    var i;
-    for (i = 0; i < this.length; i++) {
-        func(this[i], i, this);
-    }
-};
 
-Array.prototype.map = function(func, sobj) {
-    var i, a = [];
-    for (i = 0; i < this.length; i++) {
-        a[a.length] = func(this[i], i, this);
-    }
-    return a;
-};
+if(!Array.prototype.forEach) {
+	Array.prototype.forEach = function(func, sobj) {
+		var i;
+		for (i = 0; i < this.length; i++) {
+			func(this[i], i, this);
+		}
+	};
+}
 
-Array.prototype.filter = function(func, sobj) {
-    var i, a = [], f;
-    for (i = 0; i < this.length; i++) {
-        f = func(this[i], i, this);
-        if (f) {
-            a[a.length] = this[i];
-        }
-    }
-    return a;
-};
+if (!Array.prototype.map) {
+	Array.prototype.map = function(func, sobj) {
+		var i, a = [];
+		for (i = 0; i < this.length; i++) {
+			a[a.length] = func(this[i], i, this);
+		}
+		return a;
+	};
+}
 
-Array.prototype.every = function(func, sobj) {
-    var i, f;
-    for (i = 0; i < this.length; i++) {
-        f = func(this[i], i, this);
-        if (!f) {
-            return false;
-        }
-    }
-    return true;
-};
+if(!Array.prototype.filter) {
+	Array.prototype.filter = function(func, sobj) {
+		var i, a = [], f;
+		for (i = 0; i < this.length; i++) {
+			f = func(this[i], i, this);
+			if (f) {
+				a[a.length] = this[i];
+			}
+		}
+		return a;
+	};
+}
 
-Array.prototype.some = function(func, sobj) {
-    var i, f;
-    for (i = 0; i < this.length; i++) {
-        f = func(this[i], i, this);
-        if (f) {
-            return true;
-        }
-    }
-    return false;
-};
+if(!Array.prototype.every) {
+	Array.prototype.every = function(func, sobj) {
+		var i, f;
+		for (i = 0; i < this.length; i++) {
+			f = func(this[i], i, this);
+			if (!f) {
+				return false;
+			}
+		}
+		return true;
+	};
+}
 
-Array.prototype.indexOf = function(item, offset) {
-    var i;
-    if (offset === undefined) {
-        offset = 0;
-    }
-    for (i = offset; i < this.length; i++) {
-        if (item === this[i]) {
-            return i;
-        }
-    }
-    return -1;
-};
+if(!Array.prototype.some) {
+	Array.prototype.some = function(func, sobj) {
+		var i, f;
+		for (i = 0; i < this.length; i++) {
+			f = func(this[i], i, this);
+			if (f) {
+				return true;
+			}
+		}
+		return false;
+	};
+}
 
-Array.prototype.lastIndexOf = function(item, offset) {
-    var i;
-    if (offset === undefined) {
-        offset = this.length - 1;
-    }
-    for (i = offset; i >= 0 ; i--) {
-        if (item === this[i]) {
-            return i;
-        }
-    }
-    return -1;
-};
+if(!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function(item, offset) {
+		var i;
+		if (offset === undefined) {
+			offset = 0;
+		}
+		for (i = offset; i < this.length; i++) {
+			if (item === this[i]) {
+				return i;
+			}
+		}
+		return -1;
+	};
+}
 
-Array.isArray = function (arr) {
-    if (arr instanceof Array) {
-        return true
-    } else {
-        return false;
-    }
+if(!Array.prototype.lastIndexOf) {
+	Array.prototype.lastIndexOf = function(item, offset) {
+		var i;
+		if (offset === undefined) {
+			offset = this.length - 1;
+		}
+		for (i = offset; i >= 0 ; i--) {
+			if (item === this[i]) {
+				return i;
+			}
+		}
+		return -1;
+	};
+}
+
+if(!Array.isArray) {
+	Array.isArray = function (arr) {
+		if (arr instanceof Array) {
+			return true
+		} else {
+			return false;
+		}
+	};
 }
 
 /* TODO:
--- src/String.js
+++ src/String.js
@@ -7,14 +7,20 @@
  * @version: 0.1.0
  */
 
-String.prototype.trim = function () {
-    return this.replace(/^\s+|\s+$/g,'');
-};
+if(!String.prototype.trim) {
+	String.prototype.trim = function () {
+		return this.replace(/^\s+|\s+$/g,'');
+	};
+}
 
-String.prototype.trimRight = function () {
-    return this.replace(/\s+$/g,'');
-};
+if(!String.prototype.trimRight) {
+	String.prototype.trimRight = function () {
+		return this.replace(/\s+$/g,'');
+	};
+}
 
-String.prototype.trimLeft = function () {
-    return this.replace(/^\s+/g,'');
-};
+if(!String.prototype.trimLeft) {
+	String.prototype.trimLeft = function () {
+		return this.replace(/^\s+/g,'');
+	};
+}
